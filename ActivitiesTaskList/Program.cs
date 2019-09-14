4454d68d7e8927c713be43532ebb24e97529744f
myerscody16 <myerscody16@live.com>
2019-09-14 17:11:34 -0400

    API initial

--- ActivitiesTaskList/Program.cs
+++ ActivitiesTaskList/Program.cs
@@ -7,18 +7,29 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
 
 namespace ActivitiesTaskList
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+            // Find your Account Sid and Token at twilio.com/console
+            // DANGER! This is insecure. See http://twil.io/secure
+            const string accountSid = "ACa789c5fec567f04e0ab72683617dd828";
+            const string authToken = "4d148282e12613c36b8500584f592976";
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "Join Earth's mightiest heroes. Like Kevin Bacon.",
+                from: new Twilio.Types.PhoneNumber("+13134665096"),
+                to: new Twilio.Types.PhoneNumber("+12485080655")
+            );
+
+            Console.WriteLine(message.Sid);
+        }
     }
 }
